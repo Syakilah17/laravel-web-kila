664d3bfb0823273e7628a807b34c6f3abe64e7f8
AbiyyuNauval <raditya24ti@mahasiswa.pcr.ac.id>
2025-10-13 15:14:39 +0700

    Resource & Assets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,8 @@
 
     {{-- Bootstrap 5 CSS dari CDN untuk styling --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/custom-style.css') }}">
 </head>
 <body>
 
@@ -42,8 +44,10 @@
     <main class="container my-5">
 
         {{-- Hero Section / Jumbotron --}}
+
         <div class="p-5 mb-4 bg-light rounded-3 text-center">
             <div class="container-fluid py-5">
+                <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" width="350px">
                 <h1 class="display-5 fw-bold">{{$username}}</h1>
                 <p class="fs-4 col-md-8 mx-auto">{{$last_login}}</p>
                 <a href="#" class="btn btn-primary btn-lg mt-3">Pelajari Lebih Lanjut</a>
@@ -52,13 +56,13 @@
 
         {{-- Features Section --}}
         <div class="row text-center">
-            <h2 class="mb-4">Fitur Unggulan Kami</h2>
+            <h2 class="mb-4">Fitur Terbaik Kami</h2>
 
             {{-- Fitur 1 --}}
             <div class="col-lg-4 col-md-6 mb-4">
                 <div class="card h-100 shadow-sm">
                     <div class="card-body">
-                        <h5 class="card-title">Desain Modern</h5>
+                        <h5 class="card-title font-custom" >Desain Modern</h5>
                         <p class="card-text">Dibangun dengan Bootstrap 5 untuk memastikan tampilan yang bersih dan responsif di semua perangkat.</p>
                     </div>
                 </div>
