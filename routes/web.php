480a67a413a0264aa6220227fc279c14eed849c9
Syakilah17 <syakilah24ti@mahasiswa.pcr.ac.id>
2025-11-24 14:40:12 +0700

    p11

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+
+// Controller Imports
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PelangganController;
-
 use App\Http\Controllers\UserController;
-
 use App\Http\Controllers\LoginController;
+
 
 Route::post('question/store', [QuestionController::class, 'store'])
     ->name('question.store');
+
 
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
@@ -21,8 +22,6 @@ Route::get('/pcr', function () {
 Route::get('/matakuliah', function () {
     return 'awokwok';
 });
-
-Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
 Route::get('/nama/{param1}', function ($param1) {
     return 'Nama Saya : ' . $param1;
@@ -36,22 +35,15 @@ Route::get('/about', function () {
     return view('halaman-about');
 });
 
-Route::get('/home', [HomeController::class, 'index'])->name(name: 'home');
-//hahahahah
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
 
-//pelanggan 
 Route::resource('pelanggan', PelangganController::class);
 
-//user
 Route::resource('user', UserController::class);
+
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login.process');
-
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::resource('pelanggan', PelangganController::class);
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
